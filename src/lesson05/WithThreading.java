95f0edd40d226f4a749da3c218172d5deaa58cca
arcanrun <arcan.run@gmail.com>
2020-02-02 23:12:56 +0300

    lesson05.1

--- src/lesson05/WithThreading.java
+++ src/lesson05/WithThreading.java
@@ -15,16 +15,18 @@ public class WithThreading {
 
         long a = System.currentTimeMillis();
 
-        System.arraycopy(arr, 0, a1, 0, h);
-        System.arraycopy(arr, h, a2, 0, h);
+
+
 
 
         Runnable firstThread = new Runnable() {
             @Override
             public void run() {
+                System.arraycopy(arr, 0, a1, 0, h);
                 for (int i = 0; i < a1.length - 1; i++) {
                     a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+                System.arraycopy(a1, 0, arr, 0, h);
                 System.out.println("END 1");
             }
         };
@@ -32,10 +34,11 @@ public class WithThreading {
         Runnable secondThread = new Runnable() {
             @Override
             public void run() {
+                System.arraycopy(arr, h, a2, 0, h);
                 for (int i = 0; i < a2.length - 1; i++) {
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
-
+                System.arraycopy(a2, 0, arr, h, h);
                 System.out.println("END 2");
             }
 
@@ -48,13 +51,13 @@ public class WithThreading {
         t2.start();
         try {
             t1.join();
-            System.arraycopy(a1, 0, arr, 0, h);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         try {
             t2.join();
-            System.arraycopy(a2, 0, arr, h, h);
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
